acbde667202c76c4f7865426d69f87d73fe8044a
chirag4862 <chiragrvijay4862@gmail.com>
2022-10-16 15:37:55 +0530

    Google auth setup

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,12 +1,30 @@
 import { FcGoogle } from "react-icons/fc";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { auth } from "../../utils/firebase";
+import { useRouter } from "next/router";
 
 export default function Login() {
+  const route = useRouter();
+  // Sign in with Google
+  const googleProvider = new GoogleAuthProvider();
+  const GoogleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      route.push("/");
+    } catch (error) {
+      console.log("Error");
+    }
+  };
+
   return (
     <div className="shadow-xl mt-32 p-10 text-gray-700 rounded-lg login">
       <h2 className="text-2xl font-medium">Join Today</h2>
       <div className="py-4">
         <h3 className="py-4">Sign in with one of the providers</h3>
-        <button className="text-white bg-gray-700 w-full font-medium rounded-lg flex align-middle p-4 gap-2">
+        <button
+          onClick={GoogleLogin}
+          className="text-white bg-gray-700 w-full font-medium rounded-lg flex align-middle p-4 gap-2"
+        >
           <FcGoogle className="text-2xl" />
           Sign in with Google
         </button>
