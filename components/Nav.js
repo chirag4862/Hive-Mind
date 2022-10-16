cca68a0873a1ec24281f2d22fc81874b58069f5a
chirag4862 <chiragrvijay4862@gmail.com>
2022-10-16 19:06:41 +0530

    Implemented the Post Feature

--- components/Nav.js
+++ components/Nav.js
@@ -14,7 +14,7 @@ export default function Nav() {
       <ul className="flex items-center gap-10">
         {!user && (
           <Link href={"/auth/login"}>
-            <a className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8">
+            <a className="py-2 px-4 text-sm bg-amber-500 text-white rounded-lg font-medium ml-8">
               Join Now
             </a>
           </Link>
@@ -22,7 +22,7 @@ export default function Nav() {
         {user && (
           <div className="flex items-center gap-6">
             <Link href={"/post"}>
-              <button className="font-medium bg-cyan-500 text-white py-2 px-4 rounded-md text-sm">
+              <button className="font-medium bg-amber-500 text-white py-2 px-4 rounded-md text-sm">
                 Post
               </button>
             </Link>
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,6 +1,23 @@
 import { auth } from "../utils/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function Dashboard() {
+  const [user, loading] = useAuthState(auth);
+  const router = useRouter();
+
+  // see if user is not logged in
+  const getData = async () => {
+    if (loading) return;
+    if (!user) return router.push("/auth/login");
+  };
+
+  // Get users data
+  useEffect(() => {
+    getData();
+  }, [user, loading]);
+
   return (
     <div>
       <h1>Your Posts</h1>
