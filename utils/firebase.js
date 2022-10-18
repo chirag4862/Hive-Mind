c0962e20a090bd2794caf33a7dd1e86fa69c687f
chirag4862 <chiragrvijay4862@gmail.com>
2022-10-19 00:11:30 +0530

    env change

--- utils/firebase.js
+++ utils/firebase.js
@@ -9,13 +9,20 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+  // apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  // authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  // projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  // storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  // messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  // appId: process.env.NEXT_PUBLIC_APP_ID,
+  // measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+  apiKey: "AIzaSyDAM-6ua_Lcfi67DYZSpasnKZL5w3FyLIA",
+  authDomain: "hive-mind-9e910.firebaseapp.com",
+  projectId: "hive-mind-9e910",
+  storageBucket: "hive-mind-9e910.appspot.com",
+  messagingSenderId: "336947168047",
+  appId: "1:336947168047:web:e4b1653827393195fe97da",
+  measurementId: "G-PCMMJ4SDM2",
 };
 
 // Initialize Firebase
